9bffe294b2cce27710e8fd599a902851fe93e5ad
Mallaimy <bokormallaimy@gmail.com>
2022-03-10 18:20:05 +0100

    this commit is for the tasks 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* this is my first program in c
+* it will print a stetements
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
