38f81854811c5b1e6a9c87b4b0f9577e71b10115
Mallaimy <bokormallaimy@gmail.com>
2022-03-11 16:18:09 +0100

    this is for the ALX school on c programming task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main- all single digit numbers of base 10 starting from 0, followed by a new line.
+ *
+ *return:0
+ */
+int main(void)
+{
+	int i=0;
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return 0;
+}
