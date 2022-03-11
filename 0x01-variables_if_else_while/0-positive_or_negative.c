b280a436cf3a1f51ccfe5850536f46a5b3135c6a
Mallaimy <bokormallaimy@gmail.com>
2022-03-11 15:04:21 +0100

    this is for the ALX school on c programming task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /**
  * main - Prints if number is positive, zero or negative
@@ -14,13 +13,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
-	if (n == 0)
-                printf("%d is zero\n", n);
-	if (n < 0)
-                printf("%d is negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 
 	return (0);
 }
