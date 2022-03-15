56dcad6b9d3e0d9d78a73effb0cc2a651395bb09
Mallaimy <bokormallaimy@gmail.com>
2022-03-15 17:47:59 +0100

    this commits are for function nested loop

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,11 +3,28 @@
 /* _putchar.c */
 int _putchar(char);
 
-/*  alphabet */
+/*10 x alphabet */
 void print_alphabet(void);
 
-/* alphabet_10x */
+/* 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
 
-/* islower  */
 int _islower(int c);
+
+int _isalpha(int n);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
