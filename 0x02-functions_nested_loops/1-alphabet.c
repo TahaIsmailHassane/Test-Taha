5100fc38f2cf134e0824b7882ddce27d47cd2e23
Mallaimy <bokormallaimy@gmail.com>
2022-03-15 16:45:28 +0100

    this cimmit is for task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main- print all alphabet letters
- *
+ * function that prints the alphabet, in lowercase, followed by a new line.
  * Return: always 0
  */
 void print_alphabet(void)
