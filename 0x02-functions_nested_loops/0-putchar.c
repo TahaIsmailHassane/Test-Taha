defa0caec51faf094114df7119b4d78fccd0765c
Mallaimy <bokormallaimy@gmail.com>
2022-03-15 14:58:48 +0100

    this is first programme for the function and nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <main.h>
+/**
+ * main - entry point
+ * description-a program that prints _putchar, followed by a new line.
+ * Return:0
+ */
+int main(void)
+{
+	char C[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; c <= 9; i++)
+	{
+
+		putchar(c[i]);
+
+	}
+
+	putchar('\n')
+	return (0);
+
+}
